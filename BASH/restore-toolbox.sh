a3de5728c79f4f607d89d2ae078f89da37884d8c
QunlBlonk <92537122+QunlBlonk@users.noreply.github.com>
2025-11-19 15:30:59 +0100

    Implement archive management in restore-toolbox.sh
    Add functionality to manage archive mentions in restore-toolbox.sh

--- BASH/restore-toolbox.sh
+++ BASH/restore-toolbox.sh
@@ -59,5 +59,45 @@ TAG
 		let "i=i+1"
 	done
 	
+	taille2=`ls -1 .sh-toolbox | wc -l`
+	j=1
+	while [ $taille -ne $j ];do
+		nom_fich= `ls -1 .sh-toolbox | sed -n ${j}p` 
+		if [ $nom_fich != "archives" ]; then
+			parcours_archive=2
+			taille2=`wc -l < .sh-toolbox/archives`
+			t=1
+			while [ $parcours_archive -ne $taille2 ]; do
+				nom_archive=`sed -n ${parcours_archive}p .sh-toolbox/archives | cut -d ":" -f 1 `
+				if [ $nom_fich == $nom_archive ]; then
+					let "j=j+1"
+					t=0
+				fi
+				let "parcours_archive=parcours_archive+1"
+			done
+		else
+			let "j=j+1"
+		fi
+			
+			if [ $t -eq 1 ];then
+				echo "voulez vous créer supprimer la mention de l'archive ? (y/n)"
+				read var3
+				while [ $var3 != "y" ] && [ $var3 != "n" ]; do
+					echo "ti est con ma parole, j'ai dit y ou n"
+					read var3
+				done
+				if [ $var3 = "y" ]; then #rappel : faire l'exception pour 0
+					nbr_ligne=`wc -l .sh-toolbox/archives`
+					fichier2=`sed -s "1d" .sh-toolbox/archives`
+					cat << TAG > .sh-toolbox/archives
+$nbr_ligne
+$fichier2							
+TAG
+					echo "$nom_fich:$(date '+%Y%m%d-%H%M%S'):" >> .sh-toolbox/archives
+				fi
+			fi
+	
+	done	
+	
 
 fi
