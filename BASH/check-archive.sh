7520e42511cc6b5544b75e7f16051fd24d8d4493
QunlBlonk <92537122+QunlBlonk@users.noreply.github.com>
2025-11-25 23:46:24 +0100

    Enhance check-archive.sh to handle existing temp directory
    Added user prompt for recreating 'temp' directory if it already exists.

--- BASH/check-archive.sh
+++ BASH/check-archive.sh
@@ -23,9 +23,22 @@ echo -n "vous avez choisis l'archive "
 archive_choisis=`sed -n ${rep}p .sh-toolbox/archives | sed -E 's/(.*):.*:.*/\1/'`
 if [ ! -e "./temp" ]; then
         mkdir temp
+        else
+        echo "voulez vous effacer et recréer le fichier temp ?"
+        read effacer
+        while [ "$effacer" != 'y' ] && [ "$effacer" != 'n' ];do
+                echo "voulez vous effacer et recréer le fichier temp ?(y/n)"
+                read effacer
+        done
+        if [ "$effacer" == 'y' ]; then
+                rm -dr temp
+                mkdir temp
+        fi
 fi
 echo $archive_choisis
 tar -xzf $archive_choisis -C "./temp"
-if [ $? -eq 0 ]; then
-        echo "archibe décomprésé"
-fi
+
+echo "archive décomprésé"
+
+date_admin=`cat temp/var/log/auth.log | grep ' admin ' | sed -n 1p | sed -E 's/(.* [0-9]{2} .*) .* .*: .*/\1/'`
+echo $date_admin
