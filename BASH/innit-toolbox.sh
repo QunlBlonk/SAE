89a1ab225b4d7cabe34579532c9d0e1ea33de252
QunlBlonk <92537122+QunlBlonk@users.noreply.github.com>
2025-12-01 10:48:55 +0100

    Update innit-toolbox.sh

--- BASH/innit-toolbox.sh
+++ BASH/innit-toolbox.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-    if [ ! -d .sh-toolbox ]; then
+    if [ ! -d .sh-toolbox ]; then #check si le dossier .sh-toolobx existe pas
     	echo "le dossier en question va être créé"
-    	mkdir .sh-toolbox
+    	mkdir .sh-toolbox #si  vrai, le créer
     else
-    	if [ ! -d .sh-toolbox ]; then
+    	if [ ! -d .sh-toolbox ]; then #check si le dossier existe bien arpès la commande mkdir
     		exit 1;
     	fi
     fi
     
     
-    if [ ! -f .sh-toolbox/archives ]; then
+    if [ ! -f .sh-toolbox/archives ]; then #check si le fichier archives existe pas
     	echo "le fichier archives va être créé"
-    	touch .sh-toolbox/archives
+    	touch .sh-toolbox/archives #si vrai, le créer
     else
-    	if [ ! -f .sh-toolbox/archives ]; then
+    	if [ ! -f .sh-toolbox/archives ]; then #check si le fichier existe bien arpès la commande touch
     		exit 1
     	fi
     fi
     
-    if [ `ls .sh-toolbox | wc -l` -ne 1 ];then
+    if [ `ls .sh-toolbox | wc -l` -ne 1 ]; then #check si il y a plus d'un fichier (archives de base) dans le dossier
     	exit 2;
     fi
     
