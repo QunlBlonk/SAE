3095a9d8990eaabb25096b899a16a63432b10a14
ShadowLou454 <elouanlagarrigue@outlook.fr>
2025-12-11 12:12:44 +0100

    Update cipher.c

--- BASH/C/cipher.c
+++ BASH/C/cipher.c
@@ -4,17 +4,17 @@
 
 char alphabet_base64[64]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-int placement_alphabet(char lettre){
+int placement_alphabet(char lettre){ #Parcours du tableau pour retrouver l'indice de l'indice de la lettre recherchée
 	
     int indice = 0;
-    while (lettre != alphabet_base64[indice] && indice < 64){
+    while (lettre != alphabet_base64[indice] && indice < 64){ 
     	indice=indice+1;
     }
     if (lettre == alphabet_base64[indice]) return indice;
     else return -1;
 }
 
-void vigenere(char tableau[][64]){
+void vigenere(char tableau[][64]){ #utilisation du tableau de vigenere pour les futurs cryptage et décryptage
     for (int y = 0; y < 64; y++){
         for (int x = 0; x < 64; x++){
             tableau[y][x] = ((x + y) % 64) ;
@@ -42,7 +42,7 @@ char * rajout(char * mot, char * ajout){
     char * mot_final = (char *)calloc(taille_mot_final, sizeof(char));
     char * extension = (char *)calloc(20, sizeof(char));
 
-    for (i=0; i < taille_mot; i++){
+    for (i=0; i < taille_mot; i++){ #Vérification d'une présence possible d'un point dans l'ajout
         if (mot[i]=='.') {
             point=i;
         } 
@@ -70,7 +70,7 @@ void chiffrement(char * nom_fichier, char * clef, char tabVigenere[][64]){
 
     FILE * fichier = fopen(nom_fichier, "r");
     
-    char * nom_fichier_chiffrer = rajout(nom_fichier, "Chiffrer");
+    char * nom_fichier_chiffrer = rajout(nom_fichier, "Chiffrer"); #On créera un nouveau fichier chiffré avec "Chiffrer" derrière le nom de base
     remove(nom_fichier_chiffrer);
     FILE * fichierClean=fopen(nom_fichier_chiffrer, "a");
 
@@ -82,7 +82,7 @@ void chiffrement(char * nom_fichier, char * clef, char tabVigenere[][64]){
         printf("%c %d\n",clef[i%longeurClef], place_clef);
         ligne_tableau(place_lettre, tabVigenere);
 
-        if (place_lettre != -1) fputc(alphabet_base64[tabVigenere[place_lettre][place_clef]], fichierClean);
+        if (place_lettre != -1) fputc(alphabet_base64[tabVigenere[place_lettre][place_clef]], fichierClean); #Après avoir fait le chiffrement de la lettre, on ajoute sa version cryptée dans notre nouveau fichier, le retour à la ligne et les = sont comptés aussi juste après
         if (lettre == '=') fputc('=', fichierClean);
         if (lettre == '\n') fputc('\n',fichierClean);
         i++; lettre = fgetc(fichier);
